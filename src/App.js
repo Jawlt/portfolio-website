5a7e19c6b75ea4a4388aef00d1c24b8cef2b25b3
Jwalant Patel <142465606+Jawlt@users.noreply.github.com>
2024-07-10 19:04:46 -0400

    removed functionality for smaller devices

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import Home from './components/Home';
 import Projects from './components/Projects';
@@ -8,6 +8,8 @@ import { Element } from 'react-scroll';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 
 function App() {
+  const [isSmallScreen, setIsSmallScreen] = useState(false);
+
   useEffect(() => {
     const light = document.querySelector(".light");
 
@@ -18,36 +20,51 @@ function App() {
       }
     };
 
+    const handleResize = () => {
+      setIsSmallScreen(window.innerWidth <= 1080);
+    };
+
     document.addEventListener('mousemove', handleMouseMove);
+    window.addEventListener('resize', handleResize);
+    handleResize(); // Check initial screen size
 
     return () => {
       document.removeEventListener('mousemove', handleMouseMove);
+      window.removeEventListener('resize', handleResize);
     };
   }, []);
 
   return (
-    <div>  
-      <div className="container">
-        <header id="hex-grid">
-          <div className="light"></div>
-          <div className="grid"></div>
-        </header>
-      </div>
-      <div className="App">
-        <Helmet>
-          <meta charSet="utf-8" />
-          <title>Jwalant Patel</title>
-          <link rel="canonical" href="http://jawlt.ca/" />
-          <meta name="description" content="Portfolio Website" />
-        </Helmet>
-        <Navbar />
-        <Element name="home" className="section">
-          <Home />
-        </Element>
-        <Element name="projects" className="section" id="projects">
-          <Projects />
-        </Element>
-      </div>
+    <div>
+      {isSmallScreen ? (
+        <div className="small-screen-message">
+          This website is not available on smaller devices. Please use a larger screen to view the content.
+        </div>
+      ) : (
+        <div>
+          <div className="container">
+            <header id="hex-grid">
+              <div className="light"></div>
+              <div className="grid"></div>
+            </header>
+          </div>
+          <div className="App">
+            <Helmet>
+              <meta charSet="utf-8" />
+              <title>Jwalant Patel</title>
+              <link rel="canonical" href="http://jawlt.ca/" />
+              <meta name="description" content="Portfolio Website" />
+            </Helmet>
+            <Navbar />
+            <Element name="home" className="section">
+              <Home />
+            </Element>
+            <Element name="projects" className="section" id="projects">
+              <Projects />
+            </Element>
+          </div>
+        </div>
+      )}
     </div>
   );
 }

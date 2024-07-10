696e31caf1c0dac9ea5df8a9cacc3cf7c6c356c4
Jwalant Patel <142465606+Jawlt@users.noreply.github.com>
2024-07-10 04:15:18 -0400

    added github links to all projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,45 +1,53 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import './Projects.css';
-import minesweeperImage from './files/minesweeper finish.png'; // Update the path as per your structure
-import spellCheckerImage from './files/spellchecker dark.png'; // Add the other images similarly
+import minesweeperImage from './files/minesweeper finish.png';
+import spellCheckerImage from './files/spellchecker dark.png';
 import spotifyImage from './files/spotify playlist merger.png';
 import greenhouseImage from './files/mini greenhouse 0.jpg';
+import tempPicture from './files/temp picture.png';
 
 const projects = [
   {
     title: "Minesweeper Game",
     image: minesweeperImage,
-    description: "This is a brief description of Project 1."
+    description: "A modern remake of the classic Minesweeper game. The aim is to deliver a polished, user-friendly experience that retains the charm of the original game.",
+    github: "https://github.com/Jawlt/Minesweeper"
   },
   {
     title: "SpellChecker App",
     image: spellCheckerImage,
-    description: "This is a brief description of Project 2."
+    description: "This project is designed to accurately check and correct spelling in text, featuring an intuitive user interface. The aim is to provide a user-friendly tool for improving written communication.",
+    github: "https://github.com/Jawlt/SpellChecker"
   },
   {
-    title: "Spotify Guest Playlist Merger",
+    title: "Spotify Playlist Merger",
     image: spotifyImage,
-    description: "This is a brief description of Project 3."
+    description: "This project is a Spotify playlist merger developed for house parties. It allows the users to combine multiple guest playlists into one, ensuring a great music experience for all guests. The application uses an easy-to-use interface, making it simple to create the perfect party playlist from various sources.",
+    github: "https://github.com/Jawlt/Spotify-Controller"
   },
   {
     title: "Mini Greenhouse",
     image: greenhouseImage,
-    description: "This is a brief description of Project 4."
+    description: "This project is designed to help grow all types of plants. It features adjustable parameters, allowing user to control and optimize the growing conditions for various plants. The goal is to replicate various environments.",
+    github: "https://github.com/Jawlt/mini-greenhouse"
   },
   {
     title: "TO BE ADDED",
-    image: "https://picsum.photos/400",
-    description: "This is a brief description of Project 5."
+    image: tempPicture,
+    description: "This is project is still being developed.",
+    github: "https://github.com/Jawlt"
   },
   {
     title: "TO BE ADDED",
-    image: "https://picsum.photos/300",
-    description: "This is a brief description of Project 6."
+    image: tempPicture,
+    description: "This is project is still being developed.",
+    github: "https://github.com/Jawlt"
   },
 ];
 
 function Projects() {
   const [expandedCard, setExpandedCard] = useState(null);
+  const cardRefs = useRef([]);
 
   const handleCardClick = (index) => {
     setExpandedCard(expandedCard === index ? null : index);
@@ -47,11 +55,12 @@ function Projects() {
 
   return (
     <div className="projects-container">
-      <h1>Projects</h1>
+      <h1>My Projects</h1>
       <div className="projects-grid">
         {projects.map((project, index) => (
           <div
             key={index}
+            ref={el => cardRefs.current[index] = el}
             className={`project-card ${expandedCard === index ? 'expanded' : expandedCard !== null ? 'scaled-down' : ''}`}
             onClick={() => handleCardClick(index)}
           >
@@ -59,7 +68,10 @@ function Projects() {
             <div className="project-content">
               <h2 className="project-title">{project.title}</h2>
               {expandedCard === index && (
-                <p className="project-description">{project.description}</p>
+                <React.Fragment>
+                  <h3 className="project-description">{project.description}</h3>
+                  <a href={project.github} className="github" target="_blank" rel="noopener noreferrer"><i className="fa-brands fa-github"></i></a>
+                </React.Fragment>
               )}
             </div>
           </div>
